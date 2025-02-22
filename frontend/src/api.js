9191fc18a77c26ad28df52e9d0fe0301b5d9776e
VIKAS PAWAR <155407261+vikasp07@users.noreply.github.com>
2025-02-22 12:57:32 +0530

    Update api.js

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:5000/api/auth";
+const API_URL = "https://game-theraphy.onrender.com/api/auth";
 
 export const registerUser = async (userData) => {
   return axios.post(`${API_URL}/register`, userData);
